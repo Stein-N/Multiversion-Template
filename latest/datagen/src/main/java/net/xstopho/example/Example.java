54a54857506d9ebd1a10cb83752dd0dfbdeb736d
Stein-N <nico-stein-98@sbvl.de>
2025-07-21 15:18:09 +0200

    backup

---
+++ latest/datagen/src/main/java/net/xstopho/example/Example.java
@@ -0,0 +1,25 @@
+package net.xstopho.example;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
+
+@Mod(ExampleConstants.MOD_ID)
+public class Example {
+
+    public Example(IEventBus bus) {
+        // Block Registry
+        // Item Registry
+    }
+
+    @EventBusSubscriber(modid = ExampleConstants.MOD_ID)
+    public static class ExampleDatagen {
+
+        @SubscribeEvent
+        public static void generateData(GatherDataEvent.Client event) {
+            // event.createProvider(RecipeClass.Runner::new);
+        }
+    }
+}
