47959b0c6d2cf47787830cb134e287bf0d87ebe2
Stein-N <nico-stein-98@sbvl.de>
2025-07-21 18:55:16 +0200

    make latest runnable

---
+++ latest/neoforge/src/main/java/net/xstopho/example/Example.java
@@ -0,0 +1,10 @@
+package net.xstopho.example;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(ExampleConstants.MOD_ID)
+public class Example {
+
+    public Example(IEventBus bus) {}
+}
